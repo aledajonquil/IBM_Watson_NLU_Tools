ef2afbc2594edd36389d2b7f06c48679314eb278
Aleda Jonquil <116307454+aledajonquil@users.noreply.github.com>
2024-03-20 08:14:19 -0400

    Update convert_csv_to_json.py

--- convert_csv_to_json.py
+++ convert_csv_to_json.py
@@ -1,1 +1,21 @@
+import json
+import csv 
 
+INPUT_FILE = 'your-input-file.csv'
+OUTPUT_FILE = 'resulting-filename.json'
+
+nlu_training_data = []
+
+with open(INPUT_FILE, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        text = row[0]
+        classification = row[1]
+        nlu_training_data += [{
+            'labels': [classification],
+            'text': text
+        }]
+
+f = open(OUTPUT_FILE, 'w')
+f.write(json.dumps(nlu_training_data, indent=2))
+f.close()
